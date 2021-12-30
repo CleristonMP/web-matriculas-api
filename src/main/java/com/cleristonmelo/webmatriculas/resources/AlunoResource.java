c7adde8317f903a09a5fff5dd46384db690dd9cc
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2021-12-30 12:11:08 -0300

    CRUD completo de Aluno

--- src/main/java/com/cleristonmelo/webmatriculas/resources/AlunoResource.java
+++ src/main/java/com/cleristonmelo/webmatriculas/resources/AlunoResource.java
@@ -1,12 +1,20 @@
 package com.cleristonmelo.webmatriculas.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cleristonmelo.webmatriculas.dtos.AlunoDTO;
 import com.cleristonmelo.webmatriculas.services.AlunoService;
@@ -22,5 +30,31 @@ public class AlunoResource {
 	public ResponseEntity<Page<AlunoDTO>> findAll(Pageable pageable) {
 		Page<AlunoDTO> page = service.findAllPaged(pageable);
 		return ResponseEntity.ok().body(page);
+	}
+	
+	@GetMapping(value = "/{matricula}")
+	public ResponseEntity<AlunoDTO> findByMatricula(@PathVariable Long matricula){
+		AlunoDTO dto = service.findByMatricula(matricula);	
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<AlunoDTO> insert(@RequestBody AlunoDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{matricula}")
+				.buildAndExpand(dto.getMatricula()).toUri();		
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping(value = "/{matricula}")
+	public ResponseEntity<AlunoDTO> update(@PathVariable Long matricula, @RequestBody AlunoDTO dto) {
+		dto = service.update(matricula, dto);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@DeleteMapping(value = "/{matricula}")
+	public ResponseEntity<Void> delete(@PathVariable Long matricula) {
+		service.delete(matricula);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/cleristonmelo/webmatriculas/services/AlunoService.java
+++ src/main/java/com/cleristonmelo/webmatriculas/services/AlunoService.java
@@ -1,6 +1,12 @@
 package com.cleristonmelo.webmatriculas.services;
 
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -9,6 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.cleristonmelo.webmatriculas.dtos.AlunoDTO;
 import com.cleristonmelo.webmatriculas.entities.Aluno;
 import com.cleristonmelo.webmatriculas.repositories.AlunoRepository;
+import com.cleristonmelo.webmatriculas.services.exceptions.DatabaseException;
+import com.cleristonmelo.webmatriculas.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class AlunoService {
@@ -21,5 +29,55 @@ public class AlunoService {
 		Page<Aluno> page = repository.findAll(pageable);
 		return page.map(x -> new AlunoDTO(x));
 	}
+	
+	@Transactional(readOnly = true)
+	public AlunoDTO findByMatricula(Long matricula) {
+		Optional<Aluno> obj = repository.findById(matricula);
+		Aluno entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new AlunoDTO(entity);
+	}
+	
+	@Transactional
+	public AlunoDTO insert(AlunoDTO dto) {
+		Aluno entity = new Aluno();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new AlunoDTO(entity);
+	}
 
+	@Transactional
+	public AlunoDTO update(Long id, AlunoDTO dto) {
+		try {
+			Aluno entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new AlunoDTO(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+	}
+	
+
+	private void copyDtoToEntity(AlunoDTO dto, Aluno entity) {
+		entity.setNome(dto.getNome());
+		entity.setSobrenome(dto.getSobrenome());
+		entity.setRgOuCpf(dto.getRgOuCpf());
+		entity.setDataNascimento(dto.getDataNascimento());
+		entity.setEndereco(dto.getEndereco());
+		entity.setTurma(dto.getTurma());
+		entity.setResponsavel(dto.getResponsavel());
+	}	
 }
